284d93e6b81964a29f4a7291ed17efb8fdd72396
naatiworlds <natiworlds@gmail.com>
2024-09-09 12:12:14 +0200

    change cors permisions to admit frontend desplegado 10

--- src/main/kotlin/es/natiworlds/cienpaginas/config/WebConfig.kt
+++ src/main/kotlin/es/natiworlds/cienpaginas/config/WebConfig.kt
@@ -1,6 +1,5 @@
 package es.natiworlds.cienpaginas.config
 
-
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
@@ -9,12 +8,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 class WebConfig : WebMvcConfigurer {
 
 	override fun addCorsMappings(registry: CorsRegistry) {
-		registry.addMapping("/**")
-			.allowedOrigins("https://cienpaginas.netlify.app")  // Origen de tu frontend
-			.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-			.allowedHeaders("Authorization", "Content-Type", "X-Requested-With")
-			.exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials")
-			.allowCredentials(true)  // Importante si usas cookies
+		registry.addMapping("/**")  // Permite todas las rutas
+			.allowedOrigins("https://cienpaginas.netlify.app")  // Permitir solo tu frontend
+			.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // Métodos permitidos
+			.allowedHeaders("*")  // Permitir todos los encabezados
+			.allowCredentials(true)  // Permitir cookies o autenticación basada en sesión
 	}
 }
-
