fda7a207a771965bcf6356ea716a294d56fb4509
naatiworlds <natiworlds@gmail.com>
2024-09-09 11:29:01 +0200

    change cors permisions to admit frontend desplegado 6

--- src/main/kotlin/es/natiworlds/cienpaginas/config/WebConfig.kt
+++ src/main/kotlin/es/natiworlds/cienpaginas/config/WebConfig.kt
@@ -2,16 +2,17 @@ package es.natiworlds.cienpaginas.config
 
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
+@EnableWebMvc
 class WebConfig : WebMvcConfigurer {
 
 	override fun addCorsMappings(registry: CorsRegistry) {
 		registry.addMapping("/**")
 			.allowedOrigins("https://ciendias-web.onrender.com") // Exacto dominio del frontend
 			.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-			.allowedHeaders("Authorization", "Content-Type", "X-Requested-With")
 			.allowCredentials(true)
 	}
 }
